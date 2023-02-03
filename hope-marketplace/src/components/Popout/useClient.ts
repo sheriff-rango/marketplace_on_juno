7bd13a8ff2cf4ddf215e9f1288c748aba3cd8f57
Sheriff <sheriff0613@gmail.com>
2023-02-03 21:45:54 +0800

    added debug code

--- hope-marketplace/src/components/Popout/useClient.ts
+++ hope-marketplace/src/components/Popout/useClient.ts
@@ -5,7 +5,7 @@ import { useWalletManager } from "@noahsaso/cosmodal";
 import { ChainConfigs, ChainTypes } from "../../constants/ChainTypes";
 import { AccountData } from "@cosmjs/proto-signing";
 import { TokenStatus, TokenType } from "../../types/tokens";
-// import { toast } from "react-toastify";
+import { toast } from "react-toastify";
 
 type TWasmChainClients = {
 	[key in ChainTypes]: {
@@ -32,16 +32,16 @@ const useClient = (tokens?: TokenType[]) => {
 				const chainConfig = ChainConfigs[chainType];
 				// const offlineSigner = await getOfflineSigner(chainConfig.chainId);
 				const { wallet, walletClient } = connectedWallet;
-				// toast.info(`getting offline signer ${chainType}`);
+				toast.info(`getting offline signer ${chainType}`);
 				const offlineSigner = await wallet.getOfflineSignerFunction(
 					walletClient
 				)(chainConfig.chainId);
-				// toast.info(`getting account ${chainType}`);
+				toast.info(`getting account ${chainType}`);
 				const account = await offlineSigner?.getAccounts();
 				let wasmChainClient = null;
 				if (offlineSigner) {
 					try {
-						// toast.info(`getting wasm client ${chainType}`);
+						toast.info(`getting wasm client ${chainType}`);
 						wasmChainClient =
 							await SigningCosmWasmClient.connectWithSigner(
 								chainConfig.rpcEndpoint,
@@ -79,16 +79,16 @@ const useClient = (tokens?: TokenType[]) => {
 				const tokenStatus = TokenStatus[token];
 				const chain = tokenStatus.chain;
 				try {
-					// toast.info(`getting client start ${key}`);
+					toast.info(`getting client start ${key}`);
 					const client = await getClient(chain);
-					// toast.info(`getting client success ${key}`);
+					toast.info(`getting client success ${key}`);
 					setWasmClients((prev) => ({
 						...prev,
 						[chain]: client,
 					}));
 				} catch (e) {
 					console.log(e);
-					// toast.error(`getting client error ${key}`);
+					toast.error(`getting client error ${key}`);
 				}
 			});
 	}, [getClient]);
